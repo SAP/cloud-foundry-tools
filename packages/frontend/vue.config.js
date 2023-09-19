bb87f1d03c5637a648cbb2fed20cfdb7b2fb3961
Vered Constantin1 <vered.constantin@sap.com>
2023-09-19 09:14:08 +0300

    fix: fix yarn ci from root

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -5,4 +5,17 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
+  pages: {
+    app: {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: `index.html`,
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "app"],
+    },
+  },
 };
